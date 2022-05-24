1d496a2068c6fa6ef6c0bf1663be7ccae437cb9d
PushpakB3096 <rtpushpak@gmail.com>
2022-05-24 13:28:46 +0530

    basic table layout

--- src/components/AdminTable.tsx
+++ src/components/AdminTable.tsx
@@ -1,12 +1,36 @@
+import { Member } from "../interfaces";
+
 interface AdminTableProps {
-  members: any;
+  members: Member[];
 }
 
 const AdminTable = (props: AdminTableProps) => {
   const { members } = props;
-  console.log(members);
 
-  return <div>Admin Table</div>;
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <th className='table-cell'>Select </th>
+            <th className='table-cell'>Name</th>
+            <th className='table-cell'>Email</th>
+            <th className='table-cell'>Role</th>
+            <th className='table-cell'>Actions</th>
+          </tr>
+          {members.map(member => (
+            <tr key={member.id}>
+              <td className='table-cell'>{member.id}</td>
+              <td className='table-cell'>{member.name}</td>
+              <td className='table-cell'>{member.email}</td>
+              <td className='table-cell'>{member.role}</td>
+              <td className='table-cell'>Edit | Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default AdminTable;
